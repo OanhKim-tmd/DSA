dab757059ed7e6e1ccbef8bbb7e7994cbd6c2bc5
OanhKim-tmd <kimoanh16092005@gmail.com>
2025-11-27 20:06:01 +0700

    labwork2

---
+++ labwork3/e1.c
@@ -0,0 +1,19 @@
+/*Exercise 1:
+Suppose we would like to implement a queue system for a commercial website.
+Given that the website offers a list of items, each item has a limited quantity in
+stock. Implement a queue of customers, and each customer can buy only a few
+products among the available items.
+ Determine the item name, quantity, and price in stock.
+ Specify a queue of n customers; each customer can buy k products from one
+item (k is different for each customer).
+ Customers take turns to enter (enqueue) and leave (dequeue) the queue according to the FIFO order to purchase wanted products.
+ If a customer successfully purchases products, display a message and reduce
+the number of products in stock. Otherwise, display a warning message if the
+item has been run out.
+Implement the above problems in C/C++ using a Queue data structure. Write
+a main function for testing all written functions (init(), display(), enqueue(), dequeue(),...).*/
+
+#include <stdio.h>
+int main(){
+    
+}
