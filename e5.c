da2c5f0ccfcd5f2f03e39a8b821f1431f776e323
OanhKim-tmd <kimoanh16092005@gmail.com>
2025-12-06 15:04:14 +0700

    DSA

---
+++ e5.c
@@ -0,0 +1,24 @@
+/*Question 1 (10 pts)
+In this problem, a natural number is given. The objective is to find a digit and swap its position
+in the number to obtain the smallest possible value.
+Note: The smallest value must have the same number of digits as in the original value; thus,
+0 cannot be used.
+ Example: 51029 -> the possible smallest value is 15029 after one swap.
+ Propose an algorithm using iteration and implement it in C/C++ to complete the task. (3
+pts)
+ Propose and implement an alternative algorithm using recursion for this task. (6 pts)
+ Calculate the complexity of the two approaches. Justify your answer by making comments
+on the code. (1 pt)
+Hint: You can use an array to store all digits. The problem becomes sorting and searching on
+an array.*/
+
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the number n: ");
+    
+
+
+    return 0;
+}
